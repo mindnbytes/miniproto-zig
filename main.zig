54a1cdb9f5131743f98caa254c5ebd8612e49811
mindnbytes <mindnbytes@gmail.com>
2026-08-19 15:39:13 +0200

    Add command parsing and error handling

--- main.zig
+++ main.zig
@@ -3,19 +3,68 @@ const Args = struct {
     a: i32,
     b: i32,
 };
-const Command = union(enum) {
+const Name = enum {
     Ping,
-    Echo: []u8,
+    Echo,
+    Add,
+    Quit,
+};
+const Command = union(Name) {
+    Ping,
+    Echo: []const u8,
     Add: Args,
     Quit,
 };
 
-pub fn main() void {
-    const command: Command = .{ .Add = .{ .a = 2, .b = 3 } };
-    switch (command) {
-        Command.Ping => std.debug.print("I am Ping\n", .{}),
-        Command.Echo => std.debug.print("I am Echo\n", .{}),
-        Command.Add => |add| std.debug.print("I am Add, adding {} and {}\n", .{ add.a, add.b }),
-        Command.Quit => std.debug.print("I am Quit\n", .{}),
+const ParseError = error{ UnknownCommand, InvalidNumber };
+
+fn parse(input: []const u8) ParseError!Command {
+    var tokens = std.mem.tokenizeAny(u8, input, " ");
+    if (tokens.next()) |name_token| {
+        const name = std.meta.stringToEnum(Name, name_token) orelse {
+            return ParseError.UnknownCommand;
+        };
+        return switch (name) {
+            .Ping => Command.Ping,
+            .Quit => Command.Quit,
+            .Echo => blk: {
+                const s = tokens.rest();
+                break :blk Command{ .Echo = s };
+            },
+            .Add => blk: {
+                const aStr = tokens.next() orelse {
+                    break :blk ParseError.InvalidNumber;
+                };
+                const a = std.fmt.parseInt(i32, aStr, 10) catch {
+                    break :blk ParseError.InvalidNumber;
+                };
+
+                const bStr = tokens.next() orelse {
+                    break :blk ParseError.InvalidNumber;
+                };
+                const b = std.fmt.parseInt(i32, bStr, 10) catch {
+                    break :blk ParseError.InvalidNumber;
+                };
+                break :blk Command{ .Add = .{ .a = a, .b = b } };
+            },
+        };
+    }
+    // first call for tokens.next() == null
+    return ParseError.UnknownCommand;
+}
+
+pub fn main() !void {
+    const commands = [_][]const u8{ "Quit", "Ping", "Echo", "Echo HI! It's me", "Add 2 33", "What?", "Add 222", "Add banana 3" };
+    for (commands) |commandStr| {
+        if (parse(commandStr)) |command| {
+            switch (command) {
+                .Ping => std.debug.print("I am Ping\n", .{}),
+                .Echo => |pl| std.debug.print("I am Echo, saying {s}\n", .{pl}),
+                .Add => |add| std.debug.print("I am Add, adding {} and {}\n", .{ add.a, add.b }),
+                .Quit => std.debug.print("I am Quit\n", .{}),
+            }
+        } else |err| {
+            std.debug.print("Input: {s} gives: {}\n", .{ commandStr, err });
+        }
     }
 }
